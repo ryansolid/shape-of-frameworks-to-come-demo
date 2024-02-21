027d15946d02703f77565578c15e0be3adf1ec41
Ryan Carniato <ryansolid@gmail.com>
2024-02-21 10:40:59 -0800

    blog start

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,11 @@
-import { createSignal } from "solid-js";
 import "./app.css";
 
 export default function App() {
-  const [count, setCount] = createSignal(0);
   return (
     <main>
-      <h1>Hello world!</h1>
-      <button class="increment" onClick={() => setCount(count() + 1)}>
-        Clicks: {count()}
-      </button>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <a href="/">
+        <h1>Solidarity</h1>
+      </a>
     </main>
   );
 }
