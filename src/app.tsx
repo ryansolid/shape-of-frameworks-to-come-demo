f72ba8c0df34db4ec015e6579a9610180a4555ad
Ryan Carniato <ryansolid@gmail.com>
2024-02-21 17:59:07 -0800

    code splitting

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
 import { Route, Router } from "@solidjs/router";
 import "./app.css";
+import { Suspense, lazy } from "solid-js";
+import { getPost, getPosts } from "./lib/posts";
 
-import Posts from "./pages/posts";
-import PostView from "./pages/posts/[id]";
-import PostNew from "./pages/new";
-import { Suspense } from "solid-js";
+const Posts = lazy(() => import("./pages/posts"));
+const PostView = lazy(() => import("./pages/posts/[id]"));
+const PostNew = lazy(() => import("./pages/new"));
 
 export default function App() {
   return (
@@ -18,8 +19,12 @@ export default function App() {
         </main>
       )}
     >
-      <Route path="/" component={Posts} />
-      <Route path="/posts/:id" component={PostView} />
+      <Route path="/" component={Posts} load={() => getPosts()} />
+      <Route
+        path="/posts/:id"
+        component={PostView}
+        load={({ params }) => getPost(+params.id)}
+      />
       <Route path="/new" component={PostNew} />
     </Router>
   );
